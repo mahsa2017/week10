eb1a9916a7d3ac3be8781670c232717c2d3063b9
CodeYourFuture <mahsa2017>
2018-02-17 13:21:24 +0000

    first two exer done

---
+++ week10exer.js
@@ -0,0 +1,16 @@
+//this function prints hello for 5 times
+function printHelloFiveTimes() {
+    for (var i = 0; i < 5; i++)
+        console.log("Hello");
+}
+printHelloFiveTimes();
+////////////////////////////////////////////////////
+//says hello to mentors
+var mentors = ["Mozafar", "Rares", "Tim", "Ashleigh", "Gordon"];
+function welcomeMentors(arr) {
+    for (var i = 0; i < arr.length; i++)
+        console.log("Hello " + arr[i]);
+}
+welcomeMentors(mentors);
+///////////////////////////////////////////////////
+
